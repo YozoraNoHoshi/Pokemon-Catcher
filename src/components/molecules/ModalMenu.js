2d4a9bc881f8f8bbfdb4e635a3f2bedffd6ae58c
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-03-05 16:10:12 -0800

    Modal close on clicking outside

--- src/components/molecules/ModalMenu.js
+++ src/components/molecules/ModalMenu.js
@@ -2,13 +2,26 @@ import React, { PureComponent } from 'react';
 import MenuButton from '../atoms/MenuButton';
 import Modal from '../atoms/Modal';
 import Flex from '../atoms/Flex';
-
+//https://medium.com/@pitipatdop/little-neat-trick-to-capture-click-outside-react-component-5604830beb7f
 class ModalMenu extends PureComponent {
   constructor(props) {
     super(props);
     this.state = { open: false };
+    this.node = React.createRef();
+  }
+  componentDidMount() {
+    document.addEventListener('click', this.handleClick, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClick, false);
   }
 
+  handleClick = e => {
+    if (this.node.current.contains(e.target)) {
+      return;
+    }
+    if (this.state.open) this.toggleModal();
+  };
   toggleModal = () => {
     this.setState(prevSt => {
       return { open: !prevSt.open };
@@ -17,7 +30,7 @@ class ModalMenu extends PureComponent {
 
   render() {
     return (
-      <Flex jCenter className="modal-main">
+      <Flex jCenter ref={this.node} className="modal-main">
         <MenuButton active={this.state.open} onClick={this.toggleModal}>
           {this.props.buttonText}
         </MenuButton>
