bfa1e12e30548095de2a5d5275d21fefa032e48f
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-03-09 10:21:42 -0800

    removed comments and added api functions

--- src/api.js
+++ src/api.js
@@ -1,6 +1,34 @@
 import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
-export async function getPokemonFromHabitat(habitat) {
-  let pokemon = await axios.post(`${BASE_URL}/battle/${habitat}`);
-  return pokemon;
+
+export async function getAllHabitats() {
+  let result = await axios.get(`${BASE_URL}/habitats`);
+  return result.habitats;
+}
+export async function getHabitatInfo(habitat) {
+  let result = await axios.get(`${BASE_URL}/habitats/${habitat}`);
+  return result.habitat;
+}
+export async function getBattlePokemon(habitat) {
+  let result = await axios.get(`${BASE_URL}/habitats/${habitat}/battle`);
+  return result.pokemon;
+}
+export async function getHabitatPokemon(habitat) {
+  let result = await axios.get(`${BASE_URL}/habitats/${habitat}/pokemon`);
+  return result.pokemon;
+}
+export async function getAllPokemon() {
+  let result = await axios.get(`${BASE_URL}/pokemon`);
+  return result.pokemon;
+}
+export async function getPokemon(nameOrId, column = 'name') {
+  if (column !== 'name') column = 'id';
+  let result = await axios.get(`${BASE_URL}/pokemon/${nameOrId}`, {
+    params: { column }
+  });
+  return result.pokemon;
+}
+export async function getFullPokemon(pokemonName) {
+  let result = await axios.get(`${BASE_URL}/pokemon/${pokemonName}/habitats`);
+  return result.pokemon;
 }
