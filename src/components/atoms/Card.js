8497ea722017c5fba43fe77825ae038d56ffd48e
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-03-12 17:17:41 -0700

    Card atom includes a link to the pokedex page

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -2,13 +2,13 @@ import React, { PureComponent } from 'react';
 import Flex from './Flex';
 import styled from 'styled-components';
 import Sprite from './Sprite';
+import { Link } from '@reach/router';
 
 export const StyledCard = styled.div`
   background: lightslategray;
   border-radius: 5px;
   box-shadow: 1px 1px 2px black;
   padding: 10px;
-  margin: 5px;
   padding-bottom: 5px;
   height: 100px;
 `;
@@ -16,12 +16,22 @@ export const StyledCard = styled.div`
 class Card extends PureComponent {
   render() {
     return (
-      <StyledCard>
-        <Sprite src={this.props.sprite} alt={this.props.name} />
-        <Flex jCenter txCenter>
-          {this.props.name}
-        </Flex>
-      </StyledCard>
+      <Link
+        to={`/pokedex/${this.props.name.toLowerCase()}`}
+        state={this.props.passedState || null}
+        style={{
+          color: 'initial',
+          margin: '5px',
+          padding: 0
+        }}
+      >
+        <StyledCard>
+          <Sprite src={this.props.sprite} alt={this.props.name} />
+          <Flex jCenter txCenter>
+            {this.props.name}
+          </Flex>
+        </StyledCard>
+      </Link>
     );
   }
 }
