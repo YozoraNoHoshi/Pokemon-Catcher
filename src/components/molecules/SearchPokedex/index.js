8ec3a6d5668ff262dac87445bbf5bf04a32b22e5
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-03-16 18:06:22 -0700

    Pokedex remembers prev search until navigate

--- src/components/molecules/SearchPokedex/index.js
+++ src/components/molecules/SearchPokedex/index.js
@@ -1,5 +1,13 @@
 import React, { PureComponent } from 'react';
 import MenuButton from '../../atoms/MenuButton';
+import Flex from '../../atoms/Flex';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 25ch;
+  text-align: center;
+  margin: 0;
+`;
 
 // Can be made into a generic form component if more than one form is used
 class SearchPokedex extends PureComponent {
@@ -22,20 +30,20 @@ class SearchPokedex extends PureComponent {
 
   render() {
     return (
-      <div className="SearchPokedex">
-        <form onSubmit={this.handleSubmit}>
-          <input
+      <form onSubmit={this.handleSubmit}>
+        <Flex column alCenter>
+          <Input
             name="pokemon"
             type="text"
             onChange={this.handleChange}
             value={this.state.pokemon}
             placeholder="Pokemon name or ID (1-493)"
           />
-          <MenuButton as="button" type="submit">
+          <MenuButton cWidth={100} as="button" type="submit">
             Search
           </MenuButton>
-        </form>
-      </div>
+        </Flex>
+      </form>
     );
   }
 }
--- src/components/organisms/Pokedex/index.js
+++ src/components/organisms/Pokedex/index.js
@@ -1,41 +1,81 @@
 import React, { PureComponent } from 'react';
 import { getPokemon } from '../../../api';
 import SearchPokedex from '../../molecules/SearchPokedex';
-import Card from '../../atoms/Card';
+import DetailedCard from '../../atoms/DetailedCard';
+import Flex from '../../atoms/Flex';
 
 class Pokedex extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = { pokemon: {}, loading: false };
+    this.state = { pokemon: {}, pastSearch: [], loading: false };
   }
 
   searchPokemon = async nameOrId => {
     try {
       this.setState({ loading: true });
       let pokemon = await getPokemon(nameOrId);
-      if (pokemon)
-        this.setState({ pokemon, loading: false, foundPokemon: true });
-      else this.setState({ loading: false, foundPokemon: false });
+      if (!Array.isArray(pokemon)) {
+        this.setState(prevSt => {
+          let pastSearch = [...prevSt.pastSearch, prevSt.pokemon];
+          return { pokemon, pastSearch, loading: false, foundPokemon: true };
+        });
+      } else this.setState({ loading: false, foundPokemon: nameOrId });
     } catch (error) {
-      this.setState({ loading: false, foundPokemon: false });
+      this.setState({ loading: false, foundPokemon: nameOrId });
     }
   };
 
+  renderPastSearches = pastSearch => {
+    return pastSearch.map(p => {
+      return (
+        <DetailedCard
+          id={p.id}
+          species={p.species}
+          title={p.title}
+          flavor_text={p.flavor_text}
+          catch_rate={p.catch_rate}
+          sprite={p.sprite}
+        />
+      );
+    });
+  };
+
   render() {
-    if (this.state.loading) return <div />;
-    let pokemon = this.state.pokemon;
+    let {
+      id,
+      species,
+      title,
+      flavor_text,
+      catch_rate,
+      sprite
+    } = this.state.pokemon;
+    let noResults =
+      this.state.foundPokemon !== undefined &&
+      typeof this.state.foundPokemon === 'string';
     return (
-      <div className="Pokedex">
+      <Flex column jCenter alCenter>
         <SearchPokedex submit={this.searchPokemon} />
-        {this.state.foundPokemon && (
-          <Card
-            name={pokemon.name}
-            species={pokemon.species}
-            sprite={pokemon.sprite}
-            passedState={{ ...this.state.pokemon }}
+        {noResults && <Flex>No pokemon found.</Flex>}
+        {this.state.loading && <div />}
+        {this.state.foundPokemon === true && (
+          <DetailedCard
+            id={id}
+            species={species}
+            title={title}
+            flavor_text={flavor_text}
+            catch_rate={catch_rate}
+            sprite={sprite}
           />
         )}
-      </div>
+        {this.state.pastSearch.length > 0 && (
+          <Flex cwidth={100} column>
+            <Flex>Previous Searches:</Flex>
+            <Flex cWidth={100} row fWrap>
+              {this.renderPastSearches(this.state.pastSearch)}
+            </Flex>
+          </Flex>
+        )}
+      </Flex>
     );
   }
 }
