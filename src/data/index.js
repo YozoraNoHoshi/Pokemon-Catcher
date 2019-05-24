b198d3472c5207eb18ae875c11ec35caa1d60b3d
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-05-24 10:32:12 -0700

    added some more data

--- src/data/index.js
+++ src/data/index.js
@@ -6,6 +6,14 @@ export const POKEBALLS = {
   'master-ball': 255
 };
 
+// Change to be benchmarks - each benchmark has a higher catch rate
+export const HP_MULT = {
+  1: 'normal',
+  0.8: 'slightly-fed',
+  0.6: 'fed',
+  0.4: 'well-fed',
+  0.2: 'full'
+};
 export const statusMultiplier = {
   freeze: 2,
   sleep: 2,
@@ -19,10 +27,19 @@ export const CATCH_MESSAGES = {
   0: 'Oh no! The Pokemon broke free!',
   1: 'Aww! It appeared to be caught!',
   2: 'Argh! Almost had it!',
-  3: 'Shoot! It was so close too!'
+  3: 'Shoot! It was so close, too!', // 'Gah! It was so close, too!'
+  4: 'Gotcha! The Pokemon was caught!'
 };
 
-export const BERRIES = {};
+export const BERRIES = { 'oran-berry': 'normal' };
+export const HP_BERRIES = { 'razz-berry': 0.1 };
+
+export const BERRY_SPRITES = {
+  'razz-berry':
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/razz-berry.png',
+  'oran-berry':
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/oran-berry.png'
+};
 
 export const POKEBALL_SPRITES = {
   'poke-ball':
@@ -36,6 +53,12 @@ export const POKEBALL_SPRITES = {
   'master-ball':
     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/master-ball.png'
 };
+
+export const BAG_SPRITE =
+  'https://cdn.bulbagarden.net/upload/7/76/Professor_Birch_Bag.png';
+
+export const ESCAPE_ROPE =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/escape-rope.png';
 
 export const MISSINGNO = {
   id: '???',
