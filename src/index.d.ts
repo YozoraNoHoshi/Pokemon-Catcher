12b8e6d36d9fa2f564020e10f8b4229b2c0d8e30
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-05-26 10:32:05 -0700

    added type declarations

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-export interface Pokemon {
+export type Pokemon = {
   readonly id: number;
   readonly title: string;
   readonly flavor_text: string;
@@ -6,42 +6,43 @@ export interface Pokemon {
   readonly name: string;
   readonly species: string;
   readonly sprite: string;
-}
-export interface FlexDirection {
+};
+export type CaughtPokemon = Pokemon & { pokeball: string };
+export type FlexDirection = {
   readonly row?: boolean;
   readonly column?: boolean;
-}
-export interface Width {
+};
+export type Width = {
   readonly cWidth?: string | number;
-}
-export interface FontWeight {
+};
+export type FontWeight = {
   readonly bold?: boolean;
   readonly normal?: boolean;
   readonly thin?: boolean;
-}
-export interface FontSize {
+};
+export type FontSize = {
   readonly large?: boolean;
   readonly small?: boolean;
   readonly medium?: boolean;
-}
-export interface JustifyContent {
+};
+export type JustifyContent = {
   readonly jCenter?: boolean;
   readonly jStart?: boolean;
   readonly jEnd?: boolean;
   readonly jAround?: boolean;
   readonly jBetween?: boolean;
-}
-export interface AlignItems {
+};
+export type AlignItems = {
   readonly alCenter?: boolean;
   readonly alStart?: boolean;
   readonly alEnd?: boolean;
-}
-export interface Wrap {
+};
+export type Wrap = {
   readonly fWrap?: boolean;
   readonly noFWrap?: boolean;
-}
-export interface TextAlign {
+};
+export type TextAlign = {
   readonly txCenter?: boolean;
   readonly txLeft?: boolean;
   readonly txRight?: boolean;
-}
+};
