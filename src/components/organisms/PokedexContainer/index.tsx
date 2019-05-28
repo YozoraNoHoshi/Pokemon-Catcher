2d1cd0fd5028f5bf949ddb672440614acb7f2ec8
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-05-28 13:30:40 -0700

    ts migration

--- src/components/organisms/PokedexContainer/index.tsx
+++ src/components/organisms/PokedexContainer/index.tsx
@@ -1,17 +1,39 @@
 import { PureComponent } from 'react';
 import { getPokemon } from '../../../api';
+import { Pokemon } from '../../..';
 
-class PokedexContainer extends PureComponent {
-  constructor(props) {
+interface Props {
+  children: any;
+}
+
+interface State {
+  pokemon: Pokemon | object;
+  pastSearch: Pokemon[];
+  loading: boolean;
+  foundPokemon: boolean | string | undefined | number;
+}
+// Good candidate for a custom hook?
+class PokedexContainer extends PureComponent<Props, State> {
+  constructor(props: Props) {
     super(props);
-    this.state = { pokemon: {}, pastSearch: [], loading: false };
+    this.state = {
+      pokemon: {},
+      pastSearch: [],
+      loading: false,
+      foundPokemon: undefined
+    };
   }
-  searchPokemon = async nameOrId => {
+
+  // Rewrite this - typings are all over the place and overall isn't very clean.
+  searchPokemon = async (nameOrId: string | number) => {
     try {
       this.setState({ loading: true });
-      let pokemon = await getPokemon(nameOrId, true);
+      let pokemon: Pokemon | undefined[] = await getPokemon(
+        nameOrId,
+        '/habitats'
+      );
       if (!Array.isArray(pokemon)) {
-        this.setState(prevSt => {
+        this.setState((prevSt: any) => {
           let pastSearch =
             Object.keys(this.state.pokemon).length > 0
               ? [...prevSt.pastSearch, prevSt.pokemon]
@@ -35,9 +57,4 @@ class PokedexContainer extends PureComponent {
     });
   }
 }
-
-PokedexContainer.defaultProps = {};
-
-PokedexContainer.propTypes = {};
-
 export default PokedexContainer;
