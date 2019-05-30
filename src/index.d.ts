72bf27f52aeb01cb0677cf775e3230c4675f2028
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-05-30 16:09:47 -0700

    formatting

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,11 @@
+// Habitat-Related Types
 export type Habitat = { name: string; description: string };
+
 export type Habitats = {
   [habitat: string]: Habitat;
 };
 
+// Pokemon-Related Types
 export type Pokemon = {
   readonly id: number;
   readonly title: string;
@@ -12,34 +15,45 @@ export type Pokemon = {
   readonly species: string;
   readonly sprite: string;
 };
+
 export type CaughtPokemon = Pokemon & { pokeball: string };
+
 export type PokemonWithHabitat = Pokemon & { habitats: string };
+
 export interface MissingNo extends Pokemon {
   readonly id: number | string;
   readonly catch_rate: number | string;
 }
 
+// Item-Related Types
 export type PokeballIndex =
   | 'poke-ball'
   | 'great-ball'
   | 'ultra-ball'
   | 'park-ball'
   | 'master-ball';
-export type Pokeballs = { [pokeball in PokeballIndex]: number };
-export type PokeballSprites = { [pokeball in PokeballIndex]: string };
+
+export type Pokeballs = { readonly [pokeball in PokeballIndex]: number };
+
+export type PokeballSprites = { readonly [pokeball in PokeballIndex]: string };
+
 export type Sprites = {
-  [sprite: string]: string;
+  readonly [sprite: string]: string;
 };
 
-export type HPBerries = {
-  readonly 'razz-berry': number;
-};
+export type HPBerriesIndex = 'razz-berry';
+
+export type HPBerries = { readonly [berry in HPBerriesIndex]: number };
+
+export type CatchBerriesIndex = 'oran-berry';
 
 export type CatchBerries = {
-  readonly 'oran-berry': StatusIndex;
+  readonly [berry in CatchBerriesIndex]: StatusIndex
 };
 
 export type Berries = HPBerries & CatchBerries;
+
+// Battle-Related Types
 export type CatchMessage = {
   readonly 0: string;
   readonly 1: string;
@@ -48,33 +62,39 @@ export type CatchMessage = {
   readonly 4: string;
   [result: string]: string;
 };
-export type Status = {
-  readonly freeze: number;
-  readonly sleep: number;
-  readonly paralyze: number;
-  readonly burn: number;
-  readonly poison: number;
-  readonly normal: number;
-};
-export type StatusIndex = keyof Status;
 
+export type StatusIndex =
+  | 'freeze'
+  | 'sleep'
+  | 'paralyze'
+  | 'burn'
+  | 'poison'
+  | 'normal';
+
+export type Status = { readonly [effect in StatusIndex]: number };
+
+// CSS Props Types
 export type FlexDirection = {
   readonly row?: boolean;
   readonly column?: boolean;
 };
+
 export type Width = {
   readonly cWidth?: string | number;
 };
+
 export type FontWeight = {
   readonly bold?: boolean;
   readonly normal?: boolean;
   readonly thin?: boolean;
 };
+
 export type FontSize = {
   readonly large?: boolean;
   readonly small?: boolean;
   readonly medium?: boolean;
 };
+
 export type JustifyContent = {
   readonly jCenter?: boolean;
   readonly jStart?: boolean;
@@ -82,23 +102,28 @@ export type JustifyContent = {
   readonly jAround?: boolean;
   readonly jBetween?: boolean;
 };
+
 export type AlignItems = {
   readonly alCenter?: boolean;
   readonly alStart?: boolean;
   readonly alEnd?: boolean;
 };
+
 export type Wrap = {
   readonly fWrap?: boolean;
   readonly noFWrap?: boolean;
 };
+
 export type TextAlign = {
   readonly txCenter?: boolean;
   readonly txLeft?: boolean;
   readonly txRight?: boolean;
 };
+
 export type Style = {
   readonly style?: React.CSSProperties;
 };
+
 export type Action = {
   readonly type: string;
   readonly payload: any;
