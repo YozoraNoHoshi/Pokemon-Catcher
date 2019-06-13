434638641af5e6b0411684aae68bd351cee36f2f
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-06-12 22:47:13 -0700

    class component -> fc

--- src/components/organisms/PreBattle/index.tsx
+++ src/components/organisms/PreBattle/index.tsx
@@ -1,55 +1,46 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect, memo } from 'react';
+import { Pokemon } from '../../../types';
+import { RouteComponentProps } from '@reach/router';
 import Loading from '../../atoms/Loading';
 import Battle from '../../pages/Battle';
 import { getBattlePokemon, getPokemon } from '../../../api';
 import PalParkClosed from '../../molecules/PalParkClosed';
-import { Pokemon } from '../../../types';
-import { RouteComponentProps } from '@reach/router';
 
 interface Props extends RouteComponentProps {
   habitat: string;
   riggedPokemon?: string;
-  modifyPokemon: (pokemon: any) => void;
-}
-interface State {
-  loading: boolean;
-  pokemon: Pokemon;
-  noPokemon: boolean;
+  modifyPokemon: (pokemon: Pokemon | string | number) => void;
 }
 
-class PreBattle extends PureComponent<Props, State> {
-  static defaultProps = { habitat: '', riggedPokemon: '' };
-  static errorMessage =
-    'The Pal Park is currently closed for maintenance. We apologize for the inconvenience.';
-  constructor(props: Props) {
-    super(props);
-    this.state = { loading: true, pokemon: {} as Pokemon, noPokemon: false };
-  }
+const errorMessage =
+  'The Pal Park is currently closed for maintenance. We apologize for the inconvenience.';
 
-  async componentDidMount() {
-    await this.getBattlePokemon(this.props.riggedPokemon);
-  }
-  async getBattlePokemon(rigged: string | undefined) {
-    try {
-      let pokemon = rigged
-        ? await getPokemon(rigged)
-        : await getBattlePokemon(this.props.habitat);
-      this.setState({ pokemon, loading: false, noPokemon: false });
-    } catch (error) {
-      this.setState({ noPokemon: true, loading: false });
-    }
-  }
-  render() {
-    if (this.state.loading) return <Loading />;
-    if (this.state.noPokemon)
-      return <PalParkClosed message={PreBattle.errorMessage} />;
-    return (
-      <Battle
-        pokemon={this.state.pokemon}
-        modifyPokemon={this.props.modifyPokemon}
-      />
-    );
-  }
+function PreBattle(props: Props): JSX.Element {
+  const [loading, setLoading] = useState(true);
+  const [pokemon, setPokemon] = useState({} as Pokemon);
+  const [noPokemon, setNoPokemon] = useState(false);
+
+  useEffect(() => {
+    (async function() {
+      try {
+        let pokemon = props.riggedPokemon
+          ? await getPokemon(props.riggedPokemon)
+          : await getBattlePokemon(props.habitat);
+        setPokemon(pokemon);
+        setNoPokemon(false);
+        setLoading(false);
+      } catch (error) {
+        setNoPokemon(true);
+        setLoading(false);
+      }
+    })();
+  }, [props.riggedPokemon, props.habitat]);
+
+  if (loading) return <Loading />;
+  if (noPokemon) return <PalParkClosed message={errorMessage} />;
+  return <Battle pokemon={pokemon} modifyPokemon={props.modifyPokemon} />;
 }
 
-export default PreBattle;
+PreBattle.defaultProps = { habitat: '', riggedPokemon: '' };
+
+export default memo(PreBattle);
