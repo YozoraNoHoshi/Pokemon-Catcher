1509c1371a807f1a296b8495bd12e2a3d8338cd2
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-07-03 14:19:00 -0700

    Loading assets

--- src/components/atoms/Loading/index.tsx
+++ src/components/atoms/Loading/index.tsx
@@ -1,9 +1,13 @@
-import React, { useState, memo } from 'react';
+import React, { memo } from 'react';
 
 interface Props {}
 
 function Loading(props: Props): JSX.Element {
-  return <div className="Loading">Loading...</div>;
+  return (
+    <div className="Loading">
+      <img src="./static/pikachu.png" alt="Loading..." />
+    </div>
+  );
 }
 
 Loading.defaultProps = {};
