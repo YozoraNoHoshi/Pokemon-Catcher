fb3a5ce4162f753e1b789480db86b2e1f00a9c8f
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-07-03 10:17:54 -0700

    update flee rate formula to have min turns

--- src/helpers/calcFleeRate/index.ts
+++ src/helpers/calcFleeRate/index.ts
@@ -1,9 +1,8 @@
 /**
  * Calculates the flee rate of a Pokemon given a series of parameters
  * @param catchRate The catch rate of the Pokemon. Required.
- * @param modifier Outside modifers brought out by berries, weather conditions, etc.
+ * @param turns The turn count of the battle.
  * @param prevRate The previous flee rate. Defaults to 0 if not provided.
- * @param turns The turn count of the battle. Defaults to 0 if not provided.
  */
 export default function calcFleeRate(
   catchRate: number,
@@ -12,8 +11,9 @@ export default function calcFleeRate(
 ): number {
   const [elapsedTurns, relativeTurns] = turns;
   if (catchRate === 0) return 1;
-  if (relativeTurns < 0) return 0;
-  let x = Math.floor(Math.random() * 5) / catchRate;
+  if (elapsedTurns <= 5 || relativeTurns < 0) return 0;
+
+  let x = (100 * Math.floor(Math.random() * 5)) / catchRate;
   let fleeRate: number = (1 + Math.floor(elapsedTurns / 10) / 10) * x;
   return prevRate + Math.min(fleeRate, 2.5);
 }
